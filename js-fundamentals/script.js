545e0196c75d0934eaee7a06c8d597ef120ec704
ArtemZayarny <artyom.zayarny@gmail.com>
2024-02-22 11:23:44 +0200

    strict mode

---
+++ js-fundamentals/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Strict mode highlighting undefined variables
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // Variable with typo
+if (hasDriversLicense) console.log("I can drive :D");
+
+//Strict mode highlighting reserved variables names
+const interface = "Auto";
